3188babc4b2222e9bb7c4a92078add4ae47664ce
Marko Janošević <markojanosevicsm7@gmail.com>
2024-11-04 17:31:01 +0100

    Git Ignore changes

--- Backend/src/app/__init__.py
+++ Backend/src/app/__init__.py
@@ -11,7 +11,7 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    CORS(app, origins=['http://localhost:5174'])
+    CORS(app)
     db.init_app(app)
     migrate.init_app(app, db)
 
